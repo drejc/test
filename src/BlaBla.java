1ef146c992a129148456f1cce45df5793ffdfdb9
drejc <drey.ray@gmail.com>
2016-06-16 10:50:48 +0200

    Added bla bla class

---
+++ src/BlaBla.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+public class BlaBla {
+
+	public static int compare(String one, String two) {
+
+		if (two == null && one == null) {
+			return 0;
+		}
+
+		if (two == null) {
+			return -1;
+		}
+
+		if (one == null) {
+			return 1;
+		}
+
+		return two.compareTo(one);
+	}
+
+}
