483de698cdd39508506834dd12b71a0a81ba5d15
drejc <drey.ray@gmail.com>
2016-06-16 10:51:10 +0200

    Refactored blabla class

--- src/BlaBla.java
+++ src/BlaBla.java
@@ -5,17 +5,11 @@ public class BlaBla {
 
 	public static int compare(String one, String two) {
 
-		if (two == null && one == null) {
-			return 0;
-		}
+		if (two == null && one == null) { return 0; }
 
-		if (two == null) {
-			return -1;
-		}
+		if (two == null) { return -1; }
 
-		if (one == null) {
-			return 1;
-		}
+		if (one == null) { return 1; }
 
 		return two.compareTo(one);
 	}
